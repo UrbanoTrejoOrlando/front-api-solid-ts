80ad4234f387a6cdf4312894965087d76826c38f
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-05-23 07:40:11 -0600

    Carpeta common

--- src/common/env.ts
+++ src/common/env.ts
@@ -1,4 +1,4 @@
-const  API_URL = "http://localhost:3000/api/users"
-const API_URL_REGISTER = API_URL +"/register"
-const API_URL_LOGIN = API_URL + "/login"
+const  API_URL = "http://localhost:3000/api/users";
+const API_URL_REGISTER = API_URL +"/register";
+const API_URL_LOGIN = API_URL + "/login";
 export {API_URL, API_URL_REGISTER,API_URL_LOGIN};
