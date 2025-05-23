fe8a78f49fbe9d167dffd462256f87810a0ee9ee
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-05-23 07:45:17 -0600

    Carpeta events

--- src/events/register.ts
+++ src/events/register.ts
@@ -1,3 +1,12 @@
-const register = (email: String, password:String)=>{
+import { API_URL_REGISTER } from "../common/env"
 
+const register = async (email: String, password:String)=>{
+    return await fetch (API_URL_REGISTER,{
+        method:"POST",
+        headers:{
+            "Content-Type": "application/json",
+        },
+        body:JSON.stringify({email,password})
+    })
 }
+export default register;
